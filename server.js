d020aabbdc940723748dd702e0c0e682e49327bd
jared.bledsoe <jared.bledsoe@icloud.com>
2018-02-07 10:34:48 -0600

    Fixed smooth cam, custom textures

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const wss = new SocketServer({ server });
 var clients = [];
 var players = [];
 var grids = [];
-// var updatedGrids = [];
+var updatedGrids = [];
 
 //Init grids
 for (var x=0; x<5000; x+=50) {
@@ -42,15 +42,20 @@ wss.on('connection', (ws) => {
 wss.on('connection', function(ws) {
 
 	function close() {
-		var currentId = clients.indexOf(ws);
-		for (var i=0; i<grids.length; i++) {
-			if (grids[i].owner === players[currentId].id) {
-				grids[i].reset();
+		try {
+			var currentId = clients.indexOf(ws);
+			for (var i=0; i<grids.length; i++) {
+				if (grids[i].owner === players[currentId].id) {
+					grids[i].reset();
+				}
 			}
+			clients[currentId].close();
+			clients.splice(currentId, 1);
+			players.splice(currentId, 1);
 		}
-		clients[currentId].close();
-		clients.splice(currentId, 1);
-		players.splice(currentId, 1);
+		catch (e) {
+			console.log(e);
+		}
 	}
 
     ws.on('close', function() {
@@ -62,10 +67,10 @@ wss.on('connection', function(ws) {
     });
 	
 	ws.on('message', function(e) {
-		var message = JSON.parse(e);
+		try {
+			var message = JSON.parse(e);
 
-		//Player moved
-		if (message.type == 'playerUpdate') {
+			//Get client's id
 			var currentId;
 			for (var i=0; i<players.length; i++) {
 				if (players[i].id == message.info.id) {
@@ -73,21 +78,38 @@ wss.on('connection', function(ws) {
 					break;
 				}
 			}
-			if (players[currentId]) {
-				players[currentId].moves = message.info.moves;
+
+			//Player moved
+			if (message.type == 'playerUpdate') {
+				if (players[currentId]) {
+					players[currentId].moves = message.info.moves;
+				}
 			}
+
+			else if (message.type == 'initPlayer') {
+				players[currentId].canvasWidth = message.info.canvasWidth;
+				players[currentId].canvasHeight = message.info.canvasHeight;
+			}
+		}
+		catch (e) {
+			console.log(e);
 		}
 	});
 
-	clients.push(ws);
-	players.push(new Player());
+	try {
+		clients.push(ws);
+		players.push(new Player());
 
-	//Send to new player
-	ws.send(JSON.stringify({
-		type: 'initPlayer',
-		player: players[players.length-1],
-		grids: grids
-	}));
+		//Send to new player
+		ws.send(JSON.stringify({
+			type: 'initPlayer',
+			player: players[players.length-1],
+			grids: grids
+		}));
+	}
+	catch (e) {
+		console.log(e);
+	}
 });
 
 
@@ -101,19 +123,20 @@ wss.on('connection', function(ws) {
 
 //Send grids to players
 setInterval(function() {
-	for (var i=0; i<players.length; i++) {
-        if (clients[i].readyState != clients[0].OPEN) {
-        	console.log("error asdkasdjklasdjklsadljksadljkaslkd");
-        }
-        else {
+	try {
+		for (var i=0; i<players.length; i++) {
 			players[i].update();
 			
 			clients[i].send(JSON.stringify({
 				type: 'gameUpdate',
 				player: players[i],
-				grids: players[i].gridsInView
+				updatedGrids: updatedGrids
 			}));
 		}
+		updatedGrids = [];
+	}
+	catch (e) {
+		console.log(e);
 	}
 },30);
 
@@ -127,71 +150,91 @@ setInterval(function() {
 
 
 function Player() {
-	this.x = 300;
-	this.y = 150;
+	this.stuff = false;
+	this.x = 50;
+	this.y = 50;
 	this.id = Math.random();
-	this.gridId = 402;
+	this.gridId = 101;
 	this.lastGridId = 0;
 	this.moves = [false, false, false, false];
 	this.rgb = [Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255)];
 	this.contesting = false;
-	this.smooth = [0, 0];
+	this.smooth = [0, 0, 0, 0];
+	this.canvasWidth;
+	this.canvasHeight;
+	this.smoothMoving = false;
 
-	this.gridsInView = [];
 	this.viewStartGrid = 0;
-	this.viewStartGrid = 2030;
-
+	this.viewEndGrid = 2030;
 };
 Player.prototype.update = function() {
-
-	var index;
-	for (var i=0; i<players.length; i++) {
-		if (players[i].id == this.id) {
-			index = i;
-			break;
-		}
-	}
+	var index = players.indexOf(this);
 
 	//Movement
-	// var currentGridId = this.y/50 + (this.x/50 * 100);
-	if (this.contesting === false) {
-		if (this.moves[0] && this.y>0 && grids[this.gridId-1].rock != 101) {
-			this.contesting = true;
-			grids[this.gridId-1].own(index, this.id);
-			this.smooth = ['up', grids[this.gridId-1].delay];
-		}
-		else if (this.moves[1] && this.x<4950 && grids[this.gridId+100].rock != 101) {
-			this.contesting = true;
-			grids[this.gridId+100].own(index, this.id);
-			this.smooth = ['right', grids[this.gridId+100].delay];
-		}
-		else if (this.moves[2] && this.y<4950 && grids[this.gridId+1].rock != 101) {
-			this.contesting = true;
-			grids[this.gridId+1].own(index, this.id);
-			this.smooth = ['down', grids[this.gridId+1].delay];
-		}
-		else if (this.moves[3] && this.x>0 && grids[this.gridId-100].rock != 101) {
-			this.contesting = true;
-			grids[this.gridId-100].own(index, this.id);
-			this.smooth = ['left', grids[this.gridId-100].delay];
-		}
-	}
-
-	//Which blocks to show, based on position
-	this.viewStartGrid = this.gridId-1308;
-
-	this.viewEndGrid = this.gridId+1208;
-
-	this.gridsInView = [];
-
-	for (var i=this.viewStartGrid; i<=this.viewEndGrid; i++) {
-		if (grids[i]) {
-			if (grids[i].y > this.y-450 && grids[i].y < this.y+350) {
-				this.gridsInView.push(grids[i]);
+	try {
+		if (this.contesting === false) {
+			if (this.moves[0] && this.y>0 && grids[this.gridId-1].rock != 101) {
+				this.contesting = true;
+				grids[this.gridId-1].own(index, this.id);
+				this.smooth = ['up', grids[this.gridId-1].delay, grids[this.gridId-1].x, grids[this.gridId-1].y];
+			}
+			else if (this.moves[1] && this.x<4950 && grids[this.gridId+100].rock != 101) {
+				this.contesting = true;
+				grids[this.gridId+100].own(index, this.id);
+				this.smooth = ['right', grids[this.gridId+100].delay, grids[this.gridId+100].x, grids[this.gridId+100].y];
+			}
+			else if (this.moves[2] && this.y<4950 && grids[this.gridId+1].rock != 101) {
+				this.contesting = true;
+				grids[this.gridId+1].own(index, this.id);
+				this.smooth = ['down', grids[this.gridId+1].delay, grids[this.gridId+1].x, grids[this.gridId+1].y];
+			}
+			else if (this.moves[3] && this.x>0 && grids[this.gridId-100].rock != 101) {
+				this.contesting = true;
+				grids[this.gridId-100].own(index, this.id);
+				this.smooth = ['left', grids[this.gridId-100].delay, grids[this.gridId-100].x, grids[this.gridId-100].y];
 			}
 		}
+
+		//Move camera smoothly
+	    if (typeof this.smooth[0] === 'string' && this.smoothMoving === false) {
+	    	this.smoothMoving = true;
+
+	    	var smoothCamera = setInterval(() => {
+				if (this.smooth[0] === 'up') {
+		          this.y-=2;
+		        }
+		        else if (this.smooth[0] === 'right') {
+		          this.x+=2;
+		        }
+		        else if (this.smooth[0] === 'down') {
+		          this.y+=2;
+		        }
+		        else if (this.smooth[0] === 'left') {
+		          this.x-=2;
+		        }
+
+		        if (Math.abs(this.x-this.smooth[2]) <= 6 && Math.abs(this.y-this.smooth[3]) <= 6) {
+		        	iterations = 1;
+		        	this.smoothMoving = false;
+		        	this.smooth = [0, 0, 0, 0];
+		        	clearInterval(smoothCamera);
+		        }
+	    	}, this.smooth[1]/25);
+		}
+
+		//Which blocks to show, based on position
+		this.viewStartGrid = this.gridId-Math.floor(this.canvasWidth)-Math.floor(this.canvasHeight/100)-96 > 0 ? this.viewStartGrid = this.gridId-Math.floor(this.canvasWidth)-Math.floor(this.canvasHeight/100)-96 : 0;
+		this.viewEndGrid = this.gridId+Math.floor(this.canvasWidth)+Math.floor(this.canvasHeight/100)+96 < grids.length ? this.viewEndGrid = this.gridId+Math.floor(this.canvasWidth)+Math.floor(this.canvasHeight/100)+96 : 0;
+	}
+	catch (e) {
+		console.log(e);
 	}
 };
+
+
+
+
+
 
 
 
@@ -218,68 +261,83 @@ function Grid(x, y) {
 	this.rock <= 99 ? this.delay = 900 : this.delay = 2000;
 }
 Grid.prototype.own = function(index, id) {	
+	try {
+		if (this.owner !== id) {
+			this.owner = true;
+			this.contesting = true;
 
-	if (this.owner !== id) {
-		this.owner = true;
-		this.contesting = true;
-
-	setTimeout(() => {
-		this.cracks = 1;
-	},this.delay*.33);
-
-	setTimeout(() => {
-		this.cracks = 2;
-	},this.delay*.66);
+			updatedGrids.push(this);
 
 		setTimeout(() => {
-			if (players[index]) {
-				this.owner = id;
-				this.rgb = players[index].rgb;
+			this.cracks = 1;
+			updatedGrids.push(this);
+		},this.delay*.33);
+
+		setTimeout(() => {
+			this.cracks = 2;
+			updatedGrids.push(this);
+		},this.delay*.66);
+
+			setTimeout(() => {
+				if (players[index]) {
+					this.owner = id;
+					this.rgb = players[index].rgb;
+					this.occupied = true;
+					this.delay = 200;
+					this.contesting = false;
+					grids[players[index].lastGridId].occupied = false;
+					updatedGrids.push(grids[players[index].lastGridId]);
+					updatedGrids.push(this);
+
+					players[index].lastGridId = this.gridId;				
+					players[index].gridId = this.gridId;
+					players[index].x = this.x;
+					players[index].y = this.y;
+					players[index].contesting = false;
+
+					clients[index].send(JSON.stringify({
+						type: 'claimGrid',
+						grid: this
+					}));				
+				}
+			},this.delay);
+		}
+		else if (this.owner === id) {
+			setTimeout(() => {
 				this.occupied = true;
-				this.delay = 200;
-				this.cracks = 0;
-				this.contesting = false;
 				grids[players[index].lastGridId].occupied = false;
-				
+				updatedGrids.push(grids[players[index].lastGridId]);
+				updatedGrids.push(this);
+			
 				players[index].lastGridId = this.gridId;				
 				players[index].gridId = this.gridId;
 				players[index].x = this.x;
 				players[index].y = this.y;
 				players[index].contesting = false;
-				players[index].smooth = [0, 0];
-
-				clients[index].send(JSON.stringify({
-					type: 'claimGrid',
-					grid: this
-				}));				
-			}
-		},this.delay);
+			},200);
+		}
 	}
-	else if (this.owner === id) {
-		setTimeout(() => {
-			this.occupied = true;
-			grids[players[index].lastGridId].occupied = false;
-		
-			players[index].lastGridId = this.gridId;				
-			players[index].gridId = this.gridId;
-			players[index].smooth = [0, 0];
-			players[index].x = this.x;
-			players[index].y = this.y;
-			players[index].contesting = false;
-		},200);
+	catch (e) {
+		console.log(e);
 	}
 }
 Grid.prototype.reset = function() {
-	this.owner = false;
-	this.gridId = this.y/50 + (this.x/50 * 100);
-	this.rgb;
-	this.occupied = false;
-	this.rock = Math.floor((Math.random()*100)+1);
-	this.delay;
-	this.cracks = 0;
-	this.contesting = false;
+	try {
+		this.owner = false;
+		this.gridId = this.y/50 + (this.x/50 * 100);
+		this.rgb;
+		this.occupied = false;
+		this.rock = Math.floor((Math.random()*100)+1);
+		this.delay;
+		this.cracks = 0;
+		this.contesting = false;
 
-	this.rock <= 75 ? this.delay = 300 :
-	this.rock <= 95 ? this.delay = 600 :
-	this.rock <= 99 ? this.delay = 900 : this.delay = 2000;
+		this.rock <= 75 ? this.delay = 300 :
+		this.rock <= 95 ? this.delay = 600 :
+		this.rock <= 99 ? this.delay = 900 : this.delay = 2000;
+		updatedGrids.push(this);
+	}
+	catch (e) {
+		console.log(e);
+	}
 }
